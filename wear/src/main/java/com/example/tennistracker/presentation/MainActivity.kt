d59412db4a590ce8fa1f6e86443446adbe1b3720
Robin Linden <dev@robinlinden.eu>
2025-06-04 00:48:11 +0200

    Set up logging of accelerometer and gyroscope measurements

--- wear/src/main/java/com/example/tennistracker/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/tennistracker/presentation/MainActivity.kt
@@ -5,10 +5,14 @@
 
 package com.example.tennistracker.presentation
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
@@ -26,17 +31,62 @@ import androidx.wear.tooling.preview.devices.WearDevices
 import com.example.tennistracker.R
 import com.example.tennistracker.presentation.theme.TennisTrackerTheme
 
-class MainActivity : ComponentActivity() {
+data class Measurement(val x: Float, val y: Float, val z: Float)
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    lateinit var sensorManager: SensorManager
+    lateinit var accelerometer: Sensor
+    lateinit var gyroscope: Sensor
+
+    // TODO(robinlinden): Save values, dump to a file on activity exit or something.
+    // val accelerometerValues = mutableListOf<Measurement>()
+    // val gyroscopeValues = mutableListOf<Measurement>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)!!
 
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
             WearApp("Android")
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(e: SensorEvent) {
+        // TODO(robinlinden): Only add measurement if not very near the last-added one?
+        val measurement = Measurement(e.values[0], e.values[1], e.values[2])
+        if (e.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            // accelerometerValues.add(measurement)
+            Log.w(TAG, "Accelerometer $measurement")
+        } else {
+            // gyroscopeValues.add(measurement)
+            Log.w(TAG, "Gyroscope $measurement")
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        Log.e(TAG, "Accuracy changed $sensor $accuracy")
     }
 }
 
