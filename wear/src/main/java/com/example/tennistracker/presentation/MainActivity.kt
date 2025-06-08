3a518dd75a64d7b66f9beece0c384a1b30de2197
Stefan Andersson <stefan.andersson128@gmail.com>
2025-06-08 02:13:30 +0200

    Show sensor values in wearable ui
    WIP

--- wear/src/main/java/com/example/tennistracker/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/tennistracker/presentation/MainActivity.kt
@@ -15,15 +15,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
@@ -31,7 +37,32 @@ import androidx.wear.tooling.preview.devices.WearDevices
 import com.example.tennistracker.R
 import com.example.tennistracker.presentation.theme.TennisTrackerTheme
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 data class Measurement(val x: Float, val y: Float, val z: Float)
+
+class MeasurementViewModel : ViewModel() {
+
+    fun MeasurementViewModel() {
+    }
+
+    fun setAccelMeasurement(newMeasurement: Measurement) {
+        _accelMeasurement.update { newMeasurement }
+    }
+
+    fun setGyrioMeasurement(newMeasurement: Measurement) {
+        _gyroMeasurement.update { newMeasurement }
+    }
+
+    private val _accelMeasurement = MutableStateFlow(Measurement(0.0f, 0.0f, 0.0f))
+    val accelMeasurement: StateFlow<Measurement> = _accelMeasurement.asStateFlow()
+
+    private val _gyroMeasurement = MutableStateFlow(Measurement(0.0f, 0.0f, 0.0f))
+    val gyroMeasurement: StateFlow<Measurement> = _gyroMeasurement.asStateFlow()
+}
 
 class MainActivity : ComponentActivity(), SensorEventListener {
     companion object {
@@ -46,6 +77,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     // val accelerometerValues = mutableListOf<Measurement>()
     // val gyroscopeValues = mutableListOf<Measurement>()
 
+    var measurementViewModel: MeasurementViewModel = MeasurementViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -58,7 +91,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp(measurementViewModel)
         }
     }
 
@@ -78,9 +111,11 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         val measurement = Measurement(e.values[0], e.values[1], e.values[2])
         if (e.sensor.type == Sensor.TYPE_ACCELEROMETER) {
             // accelerometerValues.add(measurement)
+            measurementViewModel.setAccelMeasurement(measurement)
             Log.w(TAG, "Accelerometer $measurement")
         } else {
             // gyroscopeValues.add(measurement)
+            measurementViewModel.setGyrioMeasurement(measurement)
             Log.w(TAG, "Gyroscope $measurement")
         }
     }
@@ -91,7 +126,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp(measurementViewModel: MeasurementViewModel = MeasurementViewModel()) {
+
     TennisTrackerTheme {
         Box(
             modifier = Modifier
@@ -100,23 +136,118 @@ fun WearApp(greetingName: String) {
             contentAlignment = Alignment.Center
         ) {
             TimeText()
-            Greeting(greetingName = greetingName)
+            SensorValuesScreen(measurementViewModel)
         }
     }
 }
 
 @Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
+fun SensorValuesScreen(measurementViewModel: MeasurementViewModel = MeasurementViewModel()) {
+
+    Column {
+        DisplayGyroValues(measurementViewModel)
+        DisplayAccelValues(measurementViewModel)
+    }
+}
+
+@Composable
+fun DisplayAccelValues(measurementViewModel: MeasurementViewModel) {
+
+    val measurementData by measurementViewModel.accelMeasurement.collectAsState()
+
+    Column {
+        Row {
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Accel"
+            )
+        }
+        Row {
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "X: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.x)
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Y: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.y)
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Z: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.z)
+            )
+        }
+    }
+}
+
+@Composable
+fun DisplayGyroValues(measurementViewModel: MeasurementViewModel) {
+
+    val measurementData by measurementViewModel.gyroMeasurement.collectAsState()
+
+    Column {
+        Row {
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Gyro "
+            )
+        }
+        Row {
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "X: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.x)
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Y: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.y)
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "Z: "
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Left,
+                text = "%.2f".format(measurementData.z)
+            )
+        }
+    }
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
