4008d4fadf38057cefc5ee69429b3389f9aa1c88
Robin Linden <dev@robinlinden.eu>
2025-07-04 15:26:49 +0200

    Remove references to the deprecated wearApp configuration
    This both causes a 'WARNING: wearApp configuration is deprecated and
    planned to be removed in AGP 9.0. Please do not add any dependencies to
    it.'-warning, and breaks due to circular references in
    ':wear:compileReleaseArtProfile'.

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -53,5 +53,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    wearApp(project(":wear"))
 }
