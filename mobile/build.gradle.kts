0185ff667fbd6b47ee4cfcc72bb999e8c2613c93
Stefan Andersson <stefan.andersson128@gmail.com>
2025-07-09 03:45:03 +0200

    Mobile app, change to use compose

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -37,7 +38,13 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.material3)
     implementation(libs.play.services.wearable)
     implementation(libs.material)
     implementation(libs.androidx.activity)
@@ -45,4 +52,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 }
---
+++ mobile/src/main/java/com/example/tennistracker/theme/Theme.kt
@@ -0,0 +1,16 @@
+// Theme for mobile app
+package com.example.tennistracker.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun TennisTrackerTheme(content: @Composable () -> Unit) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content,
+    )
+}
